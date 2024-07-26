1e8a713bde74da57f1158b67f638b66e79fc7045
Omer Khan <notomerkhan@gmail.com>
2024-07-26 08:47:22 -0400

    Forgot a slash

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   },
   output: 'export', // Enables static export
   assetPrefix: '/macgic-app', // Sets the correct base path for GitHub Pages
-  basePath: 'macgic-app', // Sets the base path for your project without trailing slash
+  basePath: '/macgic-app', // Sets the base path for your project without trailing slash
 };
 
 export default nextConfig;
