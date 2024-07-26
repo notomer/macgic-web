d5fd41cd09933cedb6faa0c30499a2f8242ed7e4
Omer Khan <notomerkhan@gmail.com>
2024-07-26 01:16:26 -0400

    idk

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true, // Add this line for static export
+  },
+  output: 'export', // Add this line for static export
+  assetPrefix: '/macgic-app/', // Add this line for GitHub Pages
+  basePath: '/macgic-app/', // Add this line for GitHub Pages
 };
 
-module.exports = nextConfig;
+export default nextConfig;
