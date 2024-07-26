4dfe2aea7a7d0e66f2c528a22242709333e71ffd
Omer Khan <notomerkhan@gmail.com>
2024-07-26 08:47:01 -0400

    Fixing file paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,8 +6,8 @@ const nextConfig = {
     unoptimized: true, // Ensures images work without optimization during export
   },
   output: 'export', // Enables static export
-  assetPrefix: '/macgic-app/', // Sets the correct base path for GitHub Pages
-  basePath: '/macgic-app/', // Sets the base path for your project
+  assetPrefix: '/macgic-app', // Sets the correct base path for GitHub Pages
+  basePath: 'macgic-app', // Sets the base path for your project without trailing slash
 };
 
 export default nextConfig;
